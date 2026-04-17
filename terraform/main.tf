a64b215d2859d036cafaddc32289e2830e3809db
c.gonzalez.andrade <c.gonzalez.andrade@accenture.com>
2026-04-17 10:46:26 +0200

    Refactor configuración de seguridad en main.tf, añadiendo grupo de seguridad por defecto y corrigiendo comandos de instalación

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,35 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_default_security_group" "default" {
+  dynamic "ingress" {
+    for_each = var.open_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-default-sg"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = var.instance_type
 
   associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_default_security_group.default.id]
 
   root_block_device {
     volume_size = var.disk_size
@@ -14,10 +38,10 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              dnf update -y
+              yum update -y
 
               # Instalar Docker
-              dnf install -y docker
+              yum install -y docker
 
               # Arrancar Docker
               systemctl start docker
