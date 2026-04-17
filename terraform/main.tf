036fd7e7a8bf3d6757d4c80d002abce8744d589a
Cristina Gonzalez <c.gonzalezandrade5@gmail.com>
2026-04-17 08:56:48 +0200

    Refactor script de user_data en main.tf, mejorando la legibilidad y organización de los comandos de instalación y configuración

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,12 +15,21 @@ resource "aws_instance" "web" {
   user_data = <<-EOF
               #!/bin/bash
               dnf update -y
+
+              # Instalar Docker
               dnf install -y docker
+
+              # Arrancar Docker
               systemctl start docker
               systemctl enable docker
 
+              # Esperar a que Docker esté listo
+              sleep 10
+
+              # Crear carpeta para la web
               mkdir -p /home/ec2-user/html
 
+              # Crear página HTML
               cat <<EOT > /home/ec2-user/html/index.html
               <!DOCTYPE html>
               <html>
@@ -28,25 +37,19 @@ resource "aws_instance" "web" {
                 <title>CloudGuard</title>
               </head>
               <body style="font-family: Arial; text-align:center; background:#f4f4f4; padding:50px;">
-
                 <div style="background:white; padding:30px; border-radius:10px; display:inline-block; box-shadow:0 4px 10px rgba(0,0,0,0.1);">
-
                   <h1 style="color:#007bff;">🚀 CloudGuard Deployment</h1>
-
                   <p>Servidor: <strong>${var.name}</strong></p>
                   <p>Estado: <span style="color:green;">✔ Activo</span></p>
-
                   <hr>
-
                   <p>Infraestructura desplegada automáticamente con:</p>
                   <p>Python + Terraform + Docker</p>
-
                 </div>
-
               </body>
               </html>
               EOT
 
+              # Ejecutar Nginx con la web
               docker run -d -p 80:80 \
                 -v /home/ec2-user/html:/usr/share/nginx/html:ro \
                 nginx
